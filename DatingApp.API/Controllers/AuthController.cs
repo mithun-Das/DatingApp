a9720ee4870a6e98256e2332672ca9e1d395a159
mithun-Das <mithun.das227@gmail.com>
2019-01-28 05:00:56 +0600

    Authrepo added

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    public class AuthController
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+
+        public class AuthController{
+            
+        }
+    }
+}
