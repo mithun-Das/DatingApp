4d2242c30d66d5ec5646e1fc7f7420e997133efc
SELISE\mithun.das <mithun.das@selise.ch>
2019-01-28 19:12:23 +0600

    AuthController update

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,14 +1,38 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+
+using DatingApp.API.Data;
 
 namespace DatingApp.API.Controllers
 {
-    public class AuthController
-    {
-        [Route("api/[controller]")]
-        [ApiController]
+    [Route("api/[controller]")]
+    [ApiController]
 
-        public class AuthController{
-            
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo; 
+
+        public AuthController(IAuthRepository repo){
+            _repo  = repo;
         }
-    }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password){
+
+            username = username.ToLower();
+
+            if(await _repo.UserExists(username))
+                return Badrequest("UserName already exist!!!");
+
+            var userToCreate = new User
+            {
+                Username = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }           
+    } 
 }
